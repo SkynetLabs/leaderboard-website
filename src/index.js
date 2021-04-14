fd8b9efc286bed59b200ee735ff2cc8d630496a0
Karol Wypchlo <kwypchlo@gmail.com>
2021-04-14 18:54:47 +0200

    some initial design

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css";
+import "@fontsource/sora/400.css"; // normal
+import "@fontsource/sora/600.css"; // semibold
+import "@fontsource/sora/800.css"; // extrabold
+import "@fontsource/source-sans-pro/400.css"; // normal
+import "@fontsource/source-sans-pro/600.css"; // semibold
+import "./assets/main.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const { rose } = require("tailwindcss/colors");
+
+const colors = {
+  primary: { light: "#33D17E", DEFAULT: "#00c65e" },
+  warning: "#ffd567",
+  error: "#ED5454",
+  palette: {
+    100: "#f5f7f7",
+    200: "#d4dddb",
+    300: "#9e9e9e",
+    400: "#555555",
+    500: "#242424",
+    600: "#0d0d0d",
+  },
+};
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    backgroundColor: (theme) => ({ ...theme("colors"), ...colors }),
+    borderColor: (theme) => ({ ...theme("colors"), ...colors }),
+    textColor: (theme) => ({ ...theme("colors"), ...colors }),
+    placeholderColor: (theme) => ({ ...theme("colors"), ...colors }),
+    extend: {
+      fontFamily: {
+        sans: ["Sora", ...defaultTheme.fontFamily.sans],
+        content: ["Source\\ Sans\\ Pro", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        rose,
+      },
+    },
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/forms")],
+};
