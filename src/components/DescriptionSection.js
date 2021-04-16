1c98c27804b154b41c510f78f2d15c0904dc64fe
Karol Wypchlo <kwypchlo@gmail.com>
2021-04-16 17:46:38 +0200

    apply 35a7d8d8740c3241f2591cfef9240d0c7e0e0bb6

--- src/components/DescriptionSection.js
+++ src/components/DescriptionSection.js
@@ -34,11 +34,18 @@ export default function DescriptionSection() {
           <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
             {/* Testimonial card*/}
             <div className="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
-              <img className="absolute inset-0 h-full w-full object-cover" src="/artwork.png" alt="Skynet Graphic" />
+              <img
+                className="absolute inset-0 h-full w-full object-cover blur-lg"
+                src="/artwork.png"
+                alt="Skynet Graphic"
+              />
               <div className="absolute inset-0 bg-rose-500" style={{ mixBlendMode: "multiply" }} />
-              <div className="absolute inset-0 bg-gradient-to-t from-primary via-primary opacity-90" />
+              <div
+                className="absolute inset-0 bg-gradient-to-t from-black via-gray-900 to-white opacity-50"
+                style={{ mixBlendMode: "multiply" }}
+              />
               <div className="relative px-8">
-                <blockquote className="mt-8" style={{ textShadow: "0px 0px 16px #000" }}>
+                <blockquote className="mt-8" style={{ textShadow: "0px 0px 14px #000" }}>
                   <div className="relative text-lg font-medium text-white md:flex-grow">
                     <svg
                       className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-rose-400"
