eb39d158e25c407559152f09303a73991e623c37
Daniel Giles Helm <dan.helm@gmail.com>
2021-04-27 15:00:43 -0500

    add MySky and UserProfile DAC

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LeaderboardPage from "./pages/LeaderboardPage";
+import { SkynetProvider } from "./state/SkynetContext";
 
 export default function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
-        <Route path="/leaderboard">
-          <LeaderboardPage />
-        </Route>
-      </Switch>
+      <SkynetProvider>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/leaderboard">
+            <LeaderboardPage />
+          </Route>
+        </Switch>
+      </SkynetProvider>
     </Router>
   );
 }
---
+++ src/components/MySkyButton.js
@@ -0,0 +1,110 @@
+import { useEffect, useContext, useState } from "react";
+// import { Button, Icon, Loader } from 'semantic-ui-react';
+// import { useStoreState, useStoreActions, useStore } from 'easy-peasy';
+import { SkynetContext } from "../state/SkynetContext";
+import { UserCircleIcon } from "@heroicons/react/outline";
+
+const MySkyButton = () => {
+  const { mySky, userProfile } = useContext(SkynetContext);
+  const [loggedIn, setLoggedIn] = useState(false); //This will get moved to global state.
+  const [loading, setLoading] = useState(true); //This will get moved to global state.
+  const [profile, setProfile] = useState();
+  const [avatar, setAvatar] = useState();
+  const [userID, setUserID] = useState();
+  // const { fetchUserID, logout } = useStoreActions((state) => state.mySky);
+  // const { loggedIn } = useStoreState((state) => state.mySky);
+  // const { store } = useStore();
+
+  useEffect(() => {
+    // if we have MySky loaded
+    setLoading(true);
+    setAvatar(null);
+    setProfile(null);
+    if (mySky) {
+      mySky.checkLogin().then((result) => {
+        if (result) {
+          handleLoginSuccess();
+        }
+        setLoading(false);
+      });
+    }
+  }, [mySky]);
+
+  useEffect(() => {
+    if (profile) {
+      const skylink = profile.avatar;
+      mySky.connector.client.getSkylinkUrl(skylink).then((avatarUrl) => {
+        setAvatar(avatarUrl + "/300");
+      });
+    }
+  }, [profile]);
+
+  const onLogin = () => {
+    setLoading(true);
+    setAvatar(null);
+    setProfile(null);
+    mySky.requestLoginAccess().then((result) => {
+      if (result) {
+        handleLoginSuccess();
+      }
+      setLoading(false);
+    });
+  };
+
+  const onLogout = () => {
+    mySky.logout();
+    setLoggedIn(false);
+  };
+
+  const handleLoginSuccess = async () => {
+    setLoggedIn(true);
+    mySky.userID().then((userID) => {
+      setUserID(userID);
+      userProfile.getProfile(userID).then((result) => {
+        console.log("profile", result);
+        setProfile(result);
+      });
+    });
+  };
+
+  return (
+    <>
+      {loading && (
+        <button className="group flex items-center px-2 py-2 text-sm font-medium rounded-md nav-link">[Spinner]</button>
+      )}
+      {!loading && !loggedIn && (
+        <button className="group flex items-center px-2 py-2 text-sm font-medium rounded-md nav-link" onClick={onLogin}>
+          <UserCircleIcon className="mr-3 h-6 w-6" aria-hidden="true" />
+          MySky Login
+        </button>
+      )}
+      {!loading && loggedIn && (
+        <>
+          <button
+            className="group flex items-center px-2 py-2 text-sm font-medium rounded-md nav-link"
+            onClick={onLogout}
+          >
+            <UserCircleIcon className="mr-3 h-6 w-6" aria-hidden="true" />
+            {profile ? "Logout: " + profile.username : "MySky Logout"}
+          </button>
+          <img src={avatar} />
+        </>
+      )}
+    </>
+  );
+};
+
+export default MySkyButton;
+
+// // Example profile object converted from SkyID profile:
+// {
+//   "version": 1,
+//   "username": "dghelm",
+//   "aboutMe": "Developer Evangelist for Skynet Labs.",
+//   "location": "Oklahoma City, OK, USA",
+//   "topics": [],
+//   "avatar": "jADT7g5u7GRJ7YfSoFGeWozCkJF2eJrJSeq9mZeu4LCiXg"
+// }
+
+// Avatar is a folder with various size images named: ["50","150","300","600","1920"]
+// If original image size is smaller than name's dimensions, the size actually the original upload size, despite the file's name.
