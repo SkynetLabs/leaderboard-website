53fc08356de9cb4e7e4b8225c4e662198a648671
Daniel Giles Helm <dan.helm@gmail.com>
2021-05-07 20:35:06 -0500

    date changes

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -23,7 +23,7 @@ export default function HeroSection() {
                 The Build phase is currently underway.
               </Subheader>
               <Paragraph>
-                The Explore phase begins on <span className="font-extrabold text-palette-600">May 7, 2021</span> and is
+                The Explore phase begins on <span className="font-extrabold text-palette-600">May 12, 2021</span> and is
                 open to the public. Win Siacoin and swag for trying out the new decentralized ecosystem! To be notified
                 for the Explore phase, sign up below:
               </Paragraph>
